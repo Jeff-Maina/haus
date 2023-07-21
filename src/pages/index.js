6db261c6d9ffda53b1a77378833b025dda9faf3d
Jeff-Maina <devyoungblud@gmail.com>
2023-07-21 12:57:30 +0300

    added page head title

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import { Hanken_Grotesk, Inter } from "next/font/google";
 import NavBar from "@/components/Navbar";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -62,107 +63,112 @@ export default function Home() {
     },
   ];
   return (
-    <main className="w-full h-auto">
-      <div className="blur-container"></div>
-      <div className="md:px-14 px-6">
-        <NavBar />
+    <>
+      <Head>
+        <title>Hotels</title>
+      </Head>
+      <main className="w-full h-auto">
+        <div className="blur-container"></div>
+        <div className="md:px-14 px-6">
+          <NavBar />
 
-        <section className="h-44 w-full items-end flex justify-between">
-          <div className="relative">
-            <h1 data-scroll className="text-[5rem] font-wulkanMedium p-0">
-              Hotels
-            </h1>
-            <h1 className="absolute font-wulkanSemibold text-[1.9rem] font-extralight top-0 -right-12">
-              (9)
-            </h1>
+          <section className="h-44 w-full items-end flex justify-between">
+            <div className="relative">
+              <h1 data-scroll className="text-[5rem] font-wulkanMedium p-0">
+                Hotels
+              </h1>
+              <h1 className="absolute font-wulkanSemibold text-[1.9rem] font-extralight top-0 -right-12">
+                (9)
+              </h1>
+            </div>
+          </section>
+          <sectior className="w-full h-auto grid lg:grid-cols-3 md:grid-cols-2 gap-10 image-grid mt-10">
+            {hotels.map((hotel, index) => (
+              <div
+                data-scroll
+                data-scroll-speed="0.5"
+                className="w-full image-card rounded-md overflow-hidden relative"
+              >
+                <img
+                  src={`${hotel.image}`}
+                  className="h-full w-full object-cover hover:scale-110 transition-all ease duration-500"
+                  alt=""
+                />
+                <div className="w-full absolute bottom-0 h-20 flex items-center pl-6 pointer-events-none">
+                  <h1
+                    data-scroll
+                    data-scroll-speed=".2"
+                    className="font-montserrat uppercase text-lg text-white z-10 relative border-r-2 pr-3 border-white"
+                  >
+                    {hotel.name}
+                  </h1>
+                  <h1 className="text-white relative z-10 pl-3 uppercase font-montserrat text-sm">
+                    {hotel.destination}
+                  </h1>
+                </div>
+              </div>
+            ))}
+          </sectior>
+        </div>
+        <div className="w-full  blur-container flex justify-center h-20 my-36 mb-6">
+          {/*  //!Should be sticky!! for some reason it's not working,locomotive scroll's styles overwriting the sticky property perhaps??????who knows */}
+          <div className="w-96 h-16 min-w-[23rem] rounded-full bg-[#222]  -translate-y-2 z-50 grid place-items-center p-4">
+            <input
+              type="text"
+              className="h-full w-full bg-transparent pl-4 font-montserrat text-white"
+              placeholder="Find your ideal hotel"
+            />
           </div>
-        </section>
-        <sectior className="w-full h-auto grid lg:grid-cols-3 md:grid-cols-2 gap-10 image-grid mt-10">
-          {hotels.map((hotel, index) => (
-            <div
-              data-scroll
-              data-scroll-speed="0.5"
-              className="w-full image-card rounded-md overflow-hidden relative"
-            >
-              <img
-                src={`${hotel.image}`}
-                className="h-full w-full object-cover hover:scale-110 transition-all ease duration-500"
-                alt=""
-              />
-              <div className="w-full absolute bottom-0 h-20 flex items-center pl-6 pointer-events-none">
+        </div>
+
+        <footer className="w-full h-screen bg-[#1d1d1d] flex flex-col">
+          <div className="w-full flex-grow lg:grid grid-cols-2">
+            <div className="w-full lg:h-full h-2/4 grid place-items-center">
+              <div className="flex flex-col items-center">
                 <h1
                   data-scroll
-                  data-scroll-speed=".2"
-                  className="font-montserrat uppercase text-lg text-white z-10 relative border-r-2 pr-3 border-white"
+                  data-scroll-speed="2"
+                  className="text-white font-wulkanSemibold lg:text-[4rem] text-[2.5rem]"
                 >
-                  {hotel.name}
+                  Stay in the know
                 </h1>
-                <h1 className="text-white relative z-10 pl-3 uppercase font-montserrat text-sm">
-                  {hotel.destination}
-                </h1>
+                <button
+                  data-scroll
+                  data-scroll-speed="2"
+                  className="bg-white lg:w-72 lg:h-14 relative grid place-items-center rounded-full p-3 px-10 button text-md mt-8 font-montserrat footer-button overflow-hidden"
+                >
+                  <div className="w-40 h-6 overflow-hidden relative grid place-items-center">
+                    <h1 className="absolute text-top">Read our Substack</h1>
+                    <h1 className="absolute text-bottom">Read our Substack</h1>
+                  </div>
+                </button>
               </div>
             </div>
-          ))}
-        </sectior>
-      </div>
-      <div className="w-full  blur-container flex justify-center h-20 my-36 mb-6">
-        {/*  //!Should be sticky!! for some reason it's not working,locomotive scroll's styles overwriting the sticky property perhaps??????who knows */}
-        <div className="w-96 h-16 min-w-[23rem] rounded-full bg-[#222]  -translate-y-2 z-50 grid place-items-center p-4">
-          <input
-            type="text"
-            className="h-full w-full bg-transparent pl-4 font-montserrat text-white"
-            placeholder="Find your ideal hotel"
-          />
-        </div>
-      </div>
-
-      <footer className="w-full h-screen bg-[#1d1d1d] flex flex-col">
-        <div className="w-full flex-grow lg:grid grid-cols-2">
-          <div className="w-full lg:h-full h-2/4 grid place-items-center">
-            <div className="flex flex-col items-center">
-              <h1
-                data-scroll
-                data-scroll-speed="2"
-                className="text-white font-wulkanSemibold lg:text-[4rem] text-[2.5rem]"
-              >
-                Stay in the know
-              </h1>
-              <button
-                data-scroll
-                data-scroll-speed="2"
-                className="bg-white lg:w-72 lg:h-14 relative grid place-items-center rounded-full p-3 px-10 button text-md mt-8 font-montserrat footer-button overflow-hidden"
-              >
-                <div className="w-40 h-6 overflow-hidden relative grid place-items-center">
-                  <h1 className="absolute text-top">Read our Substack</h1>
-                  <h1 className="absolute text-bottom">Read our Substack</h1>
-                </div>
-              </button>
+            <div className="w-full lg:h-full h-2/4 lg:border-l border-t border-zinc-500 grid place-items-center">
+              <div className="flex flex-col items-center">
+                <h1
+                  data-scroll
+                  data-scroll-speed="2"
+                  className="text-white font-wulkanSemibold lg:text-[4rem] text-[2.5rem]"
+                >
+                  Get in touch
+                </h1>
+                <button
+                  data-scroll
+                  data-scroll-speed="2"
+                  className="bg-white lg:w-72 lg:h-14 relative grid place-items-center rounded-full p-3 px-10 button text-md mt-8 font-montserrat footer-button overflow-hidden"
+                >
+                  <div className="w-36 h-6 overflow-hidden relative grid place-items-center">
+                    <h1 className="absolute text-top">Send us an Email</h1>
+                    <h1 className="absolute text-bottom">Send us an Email</h1>
+                  </div>
+                </button>
+              </div>
             </div>
           </div>
-          <div className="w-full lg:h-full h-2/4 lg:border-l border-t border-zinc-500 grid place-items-center">
-            <div className="flex flex-col items-center">
-              <h1
-                data-scroll
-                data-scroll-speed="2"
-                className="text-white font-wulkanSemibold lg:text-[4rem] text-[2.5rem]"
-              >
-                Get in touch
-              </h1>
-              <button
-                data-scroll
-                data-scroll-speed="2"
-                className="bg-white lg:w-72 lg:h-14 relative grid place-items-center rounded-full p-3 px-10 button text-md mt-8 font-montserrat footer-button overflow-hidden"
-              >
-                <div className="w-36 h-6 overflow-hidden relative grid place-items-center">
-                  <h1 className="absolute text-top">Send us an Email</h1>
-                  <h1 className="absolute text-bottom">Send us an Email</h1>
-                </div>
-              </button>
-            </div>
-          </div>
-        </div>
-        <div className="w-full h-32 border-t border-zinc-500"></div>
-      </footer>
-    </main>
+          <div className="w-full h-32 border-t border-zinc-500"></div>
+        </footer>
+      </main>
+    </>
   );
 }
